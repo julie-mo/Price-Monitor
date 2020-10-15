5591544c55f0cc9c616ed1659b914ba2d1ae2864
julie-mo <40784525+julie-mo@users.noreply.github.com>
2020-10-14 21:13:38 -0700

    update header to bypass amazon authentication

--- pricemonitor.py
+++ pricemonitor.py
@@ -1,1 +1,58 @@
-import requests #connects to urlfrom bs4 import BeautifulSoupimport smtplib #simple mail protocolimport timeURL = 'https://www.amazon.ca/Beats-Studio3-Wireless-Headphones-Collection/dp/B07JXDTL24?pf_rd_p=b2e88044-7acf-410f-9018-dc66c6df11fe&pd_rd_wg=nCQCX&pf_rd_r=KZ1F1JZ7AP8HDRAT6WK7&ref_=pd_gw_cr_simh&pd_rd_w=iUoiZ&pd_rd_r=b4674aad-6f67-4e50-baec-4632428145d8'headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}desired_price = input("Inform you when price drops below...")sentmail = Falsedef check_price():	page = requests.get(URL, headers = headers)	soup = BeautifulSoup(page.content, 'html.parser')	title = soup.find(id = 'productTitle').get_text()	title = title.strip()	price = soup.find(id = 'priceblock_ourprice').get_text() #this returns a string	price = float(price[5:13]) #this returns a number	seller = soup.find(id = 'bylineInfo').get_text()	if(price <= desired_price):		send_email(title, seller, price)def send_email(title, seller, price):	server = smtplib.SMTP('smtp.gmail.com', 587)	server.ehlo() #command sent by email server to identify itself when connecting to another email	server.starttls() #encrypts connection	server.ehlo()	server.login('juliemo1583@gmail.com', 'kxjwtfyqjfsdbedj')	subject = "Price has dropped!"	body = title + ' by ' + seller + ' is now only ' + str(price) + '! \nCheck out ' + URL + '!'	message = 'Subject: {} \n\n {}'.format(subject, body)	server.sendmail('juliemo1583@gmail.com', 					'b3w0b@ugrad.cs.ubc.ca', 					message					)        sentmail = True	print('Email has been sent!')	server.quit()while(sentmail == False):	check_price()	time.sleep(60*60*8) #checks price every 8 hours
+import requests #connects to url
+from bs4 import BeautifulSoup
+import smtplib #simple mail protocol
+import time
+
+
+URL = 'https://www.amazon.ca/BOSOBO-Paintbrushes-Watercolor-Miniature-Detailing/dp/B07GH7WGC3/ref=sr_1_2?dchild=1&keywords=paint+brush&qid=1602734635&s=amazon-devices&sr=1-2&tag='
+
+# no longer works sept 2020 as amazon blocks any illegitimate user-agents
+# headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
+'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
+'Accept-Language' : 'en-US,en;q=0.5',
+'Accept-Encoding' : 'gzip', 
+'DNT' : '1', # Do Not Track Request Header 
+'Connection' : 'close'}
+
+desired_price = float(input("Inform you when price drops below..."))
+sentmail = False
+
+def check_price():
+	page = requests.get(URL, headers = headers)
+	# soup = BeautifulSoup(page.content, 'html.parser')
+	soup = BeautifulSoup(page.content, 'html5lib')
+	title = soup.find(id = "productTitle").get_text()
+	title = title.strip()
+
+	price = soup.find(id ="priceblock_ourprice").get_text() #returns a string
+	price = float(price[5:13]) #returns a number
+	seller = soup.find(id = 'bylineInfo').get_text()
+
+	if(price <= desired_price):
+		send_email(title, seller, price)
+
+
+def send_email(title, seller, price):
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo() #command sent by email server to identify itself when connecting to another email
+	server.starttls() #encrypts connection
+	server.ehlo()
+
+	server.login('juliemo1583@gmail.com', 'nmbroykfoniebgao')
+	subject = "Price has dropped!"
+	body = title + ' by ' + seller + ' is now only ' + str(price) + '! \nCheck out ' + URL + '!'
+	message = 'Subject: {} \n\n {}'.format(subject, body)
+	server.sendmail('juliemo1583@gmail.com', 
+					'b3w0b@ugrad.cs.ubc.ca', 
+					message
+					)
+	sentmail = True
+	print('Email has been sent!')
+	server.quit()
+
+
+while(sentmail == False):
+	check_price()
+	time.sleep(60*60*8) #checks price every 8 hours
